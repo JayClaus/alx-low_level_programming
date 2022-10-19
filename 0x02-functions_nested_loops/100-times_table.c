f7a1571f20c1a47c81cbf599d3b917585526d336
JayClaus <2019mpa024707@gmail.com>
2022-10-19 05:37:20 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,44 @@
 #include "main.h"
 /**
- * print_times_table - a function that prints the n times table, starting with 0
+ * print_times_table - prints the n times table, starting with 0
  *  @n: input
  *  Return: Always 0 (Success)
  */
 
 void print_times_table(int n)
 {
-	return (0);
+	int r, m, p;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (r = 0; r <= n; r++)
+		{
+			_putchar('0');
+
+			for (m = 1; m <= n; m++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				p = r * m;
+
+				if (p <= 99)
+					_putchar(' ');
+				if (p <= 9)
+					_putchar(' ');
+
+				if (p >= 100)
+				{
+					_putchar((p / 100) + '0');
+					_putchar(((p / 10)) % 10 + '0');
+				}
+				else if (p <= 99 && p >= 10)
+				{
+					_putchar((p / 10) + '0');
+				}
+				_putchar((p % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,71 +1,28 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_to_98 - a function that prints all natural numbers from n to 98
+ * print_to_98 - a function that prints all natural numbers from n to 98,
+ *		in order separated by a comma followed by a space.
  * @n: number input
  * Return: Always 0 (Success)
  */
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n >= 98)
 	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(",");
-			}
-			n++;
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(",");
-			}
-			n--;
-		}
-	}
+
 	else
 	{
-		printf("98");
+		while (n < 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	printf("\n"0;
-
-	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
