8c2cd9c1c43bbbacdaebfbd1b9de83ce4234f2d1
JayClaus <2019mpa024707@gmail.com>
2022-11-02 04:48:23 -0700

    1-5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+/**
+ * find_sqrt - finds the natural 2 root
+ * @num: the number
+ * @root: the root
+ * Return: 0
+ */
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+}
